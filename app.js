b89aec0891c32d3f9fd19ea99a07fe17613d358c
zsoltp2 <palzsolt.2002@gmail.com>
2025-03-26 14:07:00 +0100

    changes made

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
 const blob = document.getElementById("blob");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+window.onpointermove = (event) => {
+  const { clientX, clientY } = event;
 
-window.onpointermove = event => {
-    const { clientX, clientY } = event;
-
-    blob.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-    }, { duration: 3000, fill: "forwards" });
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
 };
 
 let interval = null;
 
-document.querySelector("h1").onmouseover = e => {
-    let iterations = 0;
-    
-    interval = setInterval(() => {
-        e.target.innerText =e.target.innerText.split("")
-            .map((letter, index) => {
-                if(index < iterations) {
-                    return e.target.dataset.value[index];
-                }
-            
-                return letters[Math.floor(Math.random() * 26)]
-            })
-            .join("");
+document.querySelector("h1").onmouseover = (e) => {
+  let iterations = 0;
 
-        if(iterations >= e.target.dataset.value.length) {
-            clearInterval(iterations);
+  interval = setInterval(() => {
+    e.target.innerText = e.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iterations) {
+          return e.target.dataset.value[index];
         }
 
-        iterations += 1 / 4;
-    }, 30);
-}
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iterations >= e.target.dataset.value.length) {
+      clearInterval(iterations);
+    }
+
+    iterations += 1 / 4;
+  }, 30);
+};
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
-console.log(hiddenElements)
